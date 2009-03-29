ba11a4dc8bc872a5dd3f86db648036653365a4c7
Mark <mark.f.bennett@gmail.com>
2009-03-29 13:24:04 +1100

    Removed generated directories from repository and started adding unit tests

---
+++ clients/minecore/models/cell.js
@@ -0,0 +1,53 @@
+// ==========================================================================
+// Minecore.Cell
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  This represents a single cell on the map of a minefield.
+
+  @extends SC.Record
+  @author MarkBennett
+  @version 0.1
+*/
+Minecore.Cell = SC.Record.extend(
+/** @scope Minecore.Cell.prototype */ {
+
+  properties: ['content','isRevealed','isFlagged',],
+
+  isExploded: function() {
+    // Has this cell been revealed?
+    if (YES == this.get('isRevealed')) {
+      // Only cells with mines will explode
+      if (this.get('content').indexOf('mine') > -1) {
+        return YES;
+      }
+    }
+    return NO;
+  }.property('isRevealed'),
+
+  isSolved: function() {
+    // Is this cell a mine?
+    if (this.get('content').indexOf('mine') > -1) {
+      // Yes, it's a mine
+      // Has it been flagged
+      if (YES == this.get('isFlagged')) {
+        if (NO == this.get('isRevealed')) {
+          return YES;
+        }
+      }
+    }
+    else {
+      // Has this cell been revealed to be empty?
+      if (YES == this.get('isRevealed')) {
+        return YES;
+      }
+    }
+
+    // Otherwise always return false
+    return NO;
+  }.property('content','isFlagged','isRevealed'),
+
+}) ;
