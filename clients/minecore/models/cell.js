756290fe034b40ed96337c44d570cb67cbfda5c2
Mark <mark.f.bennett@gmail.com>
2009-03-29 13:29:40 +1100

    The cells now use a binary flag to indicate if they have a mine in them.

--- clients/minecore/models/cell.js
+++ clients/minecore/models/cell.js
@@ -15,13 +15,13 @@ require('core');
 Minecore.Cell = SC.Record.extend(
 /** @scope Minecore.Cell.prototype */ {
 
-  properties: ['content','isRevealed','isFlagged',],
+  properties: ['hasMine','isRevealed','isFlagged',],
 
   isExploded: function() {
     // Has this cell been revealed?
     if (YES == this.get('isRevealed')) {
       // Only cells with mines will explode
-      if (this.get('content').indexOf('mine') > -1) {
+      if (this.get('hasMine')) {
         return YES;
       }
     }
@@ -30,7 +30,7 @@ Minecore.Cell = SC.Record.extend(
 
   isSolved: function() {
     // Is this cell a mine?
-    if (this.get('content').indexOf('mine') > -1) {
+    if (this.get('hasMine')) {
       // Yes, it's a mine
       // Has it been flagged
       if (YES == this.get('isFlagged')) {
@@ -48,6 +48,6 @@ Minecore.Cell = SC.Record.extend(
 
     // Otherwise always return false
     return NO;
-  }.property('content','isFlagged','isRevealed'),
+  }.property('hasMine','isFlagged','isRevealed'),
 
 }) ;
